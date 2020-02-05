41599f5b1fc6f2f2656862d86cf0d1a2ecc8c602
Ian Baldwin <ian@iantbaldw.in>
2020-02-05 20:03:25 +0000

    Add bzip2 too

--- common/init.sh
+++ common/init.sh
@@ -76,9 +76,9 @@ output "Setting up python environment" --header
 if [ "$(pyenv global)" != "${PYTHON_VERSION}" ]; then
    output "Installing Python ${PYTHON_VERSION}" --start
    CONFIUGRE_OPTS="--with-openssl=$(brew --prefix openssl)" \
-	   CFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix sqlite)/include -I$(brew --prefix openssl)/include"\
-	   CPPFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix sqlite)/include -I$(brew --prefix openssl)/include" \
-	   LDFLAGS="-L$(brew --prefix zlib)/lib -L$(brew --prefix sqlite)/lib -L$(brew --prefix openssl)/lib" \
+	   CFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix sqlite)/include -I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include"\
+	   CPPFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix sqlite)/include -I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include" \
+	   LDFLAGS="-L$(brew --prefix zlib)/lib -L$(brew --prefix sqlite)/lib -L$(brew --prefix openssl)/lib -L$(brew --prefix bzip2)/lib" \
 	   pyenv install "${PYTHON_VERSION}"
     output "Installing Python ${PYTHON_VERSION}" --end
     pyenv global "${PYTHON_VERSION}"
