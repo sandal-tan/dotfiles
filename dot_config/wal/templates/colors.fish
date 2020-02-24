ac9623220ab1550a10176c56e060822d8650f414
Ian Baldwin <ian@iantbaldw.in>
2020-02-24 02:45:05 -0500

    Okay, fish colors too!

---
+++ dot_config/wal/templates/colors.fish
@@ -0,0 +1,25 @@
+set fish_color_normal {foreground.strip}
+set fish_color_command {color2.strip}
+set fish_color_param {color4.strip}
+set fish_color_redirection $fish_color_param
+set fish_color_comment {color8.strip}
+set fish_color_error {color1.strip}
+set fish_color_escape {color5.strip}
+set fish_color_operator $fish_color_escape
+set fish_color_end {color4.strip}
+set fish_color_quote {color6.strip}
+set fish_color_autosuggestion {color1.strip}
+set fish_color_user {color10.strip}
+set fish_color_host $fish_color_normal
+set fish_color_valid_path --underline
+set fish_color_cwd {color3.strip}
+set fish_color_cwd_root {color1.strip}
+set fish_color_match --background={color12.strip}
+set fish_color_search_match {color11.strip} --background={color8.strip}
+set fish_color_selection {color7.strip} --bold --background={color8.strip}
+set fish_color_cancel -r
+set fish_pager_color_prefix {color7.strip} --bold --underline
+set fish_pager_color_completion
+set fish_pager_color_description $fish_color_quote {color3.strip}
+set fish_pager_color_progress {color15.strip} --background={color6.strip}
+set fish_color_history_current --bold
